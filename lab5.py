f77044348a29544a396607b5ec7088459a42778d
aholguinr <64372371+aholguinr@users.noreply.github.com>
2022-10-25 01:23:30 -0500

    cambios ikine

--- lab5.py
+++ lab5.py
@@ -9,20 +9,9 @@ from sensor_msgs.msg import JointState
 from dynamixel_workbench_msgs.srv import DynamixelCommand
 
 __author__ = "Andres Holguin"
-#Arreglo de torques
-Torques=[1023,1023,1023,1023,1023]
 
-
-#Obtener del usuario un valor 
-
-#Valores análogos de la posición home y los casos. Se obtuvieron desde dynamixel_wizard
-PosHome=[514,510,818,512,720]
-pos1An=[444,575,751,580,512]
-pos2An=[614,410,922,409,512]
-pos3An=[205,560,630,570,512]
-pos4An=[205,660,629,666,546]
-
-
+############################################################
+#Funciones
 
 def theta_1(P_x,P_y):
     return np.arctan2(P_y,P_x)
@@ -43,7 +32,7 @@ def theta_4(beta,theta_2,theta_3):
     return beta-theta_2-theta_3
 
 def ikine4r(P_x,P_y,P_z,beta):
-    L_1=0.147
+    L_1=0.137
     L_2=0.105
     L_3=0.105
     L_4=0.095
@@ -53,12 +42,31 @@ def ikine4r(P_x,P_y,P_z,beta):
     T4=theta_4(beta,T2,T3)
     return np.array([T1,T2,T3,T4])
 
+def ConvGra2An(q,q_Home):
+    ListDouble=q+q_Home
+    for i in range(len(ListDouble)):
+        ListDouble[i]=int(np.interp(ListDouble[i],[0, 300],[0, 1023]))
+    return ListDouble
 
+def ConvRad2An(q,q_Home):
+    ListDouble=q+q_Home
+    for i in range(len(ListDouble)):
+        ListDouble[i]=int(np.interp(ListDouble[i],[0, 300*np.pi/180],[0, 1023]))
+    return ListDouble
+
+def ConvGra2Q(Gra,Home):
+
+    return 0
+
+def ConvPosArr2QArr(PosArr):
+    [r,c]=PosArr.shape
+    QArr=np.zeros([r,c])
+    for i in range(r):
+        QArr[i,:]=ikine4r(PosArr[i,0],PosArr[i,1],PosArr[i,2],PosArr[i,3])    
+    return QArr
 
 def mapAn(theta):
     return int(interp(theta,[-150, 150],[0, 1023]))
-
-
 
 #Función que cambia valores de registros de los motores del Pincher.
 def jointCommand(command, id_num, addr_name, value, time):
@@ -76,7 +84,7 @@ def jointCommand(command, id_num, addr_name, value, time):
 def callback(data):
     global PosActual
     PosActual=np.multiply(data.position,180/pi)
-    PosActual[2]=PosActual[2]-90
+    PosActual[2]=PosActual[2]-90 #Puede ser - o + dependiendo del pincher
 
 #Función que genera el subscriber para obtener los estados de las articulaciones
 def listener():
@@ -89,9 +97,8 @@ def movPartido(j,Goal,Actual):
     N=5
     delta=((Goal-Actual)/N)
     for i in range(N):
-        jointCommand('', (j+1), 'Goal_Position', int(Actual+delta*(i+1)), 0.5)
+        jointCommand('', (j+1), 'Goal_Position', int(Actual+delta*(i+1)), 0.1)
         time.sleep(0.1)
-
 
 def RutHome(PosHome):
     print('Ir a home\n')
@@ -100,8 +107,6 @@ def RutHome(PosHome):
         print('Moviento eslabon: '+str(i+1)+'\n')
         time.sleep(0.5)
     print('En home\n')
-
-
 
 #Función de impresión de opciones
 def PrintOpciones():
@@ -115,6 +120,74 @@ def PrintOpciones():
     print("7:\tDescarga de la herramienta")
     print("8:\tSalr")
 
+############################################################
+############################################################
+
+# Valores
+
+#Arreglo de torques
+Torques=[1023,1023,1023,1023,1023]
+
+#Arreglo de offsets de Home
+AngHomeGrad=np.array([150,150,60,150,150])
+AngHomeRad=np.multiply(AngHomeGrad,np.pi/180)
+
+#Ángulos de posición home con y sin herramienta montada
+HomeS=np.array([0,0,0,0,0])
+HomeC=np.array([0,0,0,0,45])
+
+#Alturas Z en metros y ángulo de ataque
+Zpiso=0.095
+Zportaherramientas=0.1
+Zseguridad=0.15
+beta=0
+
+#Valores analogos de Home
+PosAnHomeS=ConvGra2An(HomeS,AngHomeGrad)
+PosAnHomeC=ConvGra2An(HomeC,AngHomeGrad)
+
+
+#Angulos de portaherramientas
+AngPortaHerramientasArriba=ikine4r(0.1,-0.2,Zseguridad,0)
+AngPortaHerramientasAbajo=ikine4r(0.1,-0.2,Zportaherramientas,0)
+
+
+
+#Angulos para arcos de espacio de trabajo diestro
+
+lim=70*np.pi/180
+AngArcMinIzq=ikine4r(0.168,0,Zpiso,0)
+AngArcMinIzq[0]=lim
+AngArcMinDer=ikine4r(0.168,0,Zpiso,0)
+AngArcMinDer[0]=-lim
+
+AngArcMaxIzq=ikine4r(0.3,0,Zpiso,0)
+AngArcMaxIzq[0]=lim
+AngArcMaxDer=ikine4r(0.3,0,Zpiso,0)
+AngArcMaxDer[0]=-lim
+
+# Mapa de puntos
+PuntosA=np.array([[0.200,0.150,Zpiso,beta],[0.250,0.125,Zpiso,beta],[0.200,0.100,Zpiso,beta],[0.225,0.1125,Zpiso,beta],[0.225,0.1375,Zpiso,beta]])
+PuntosJ=np.array([[0.250,0.100,Zpiso,beta],[0.250,0.050,Zpiso,beta],[0.250,0.075,Zpiso,beta],[0.200,0.075,Zpiso,beta],[0.200,0.100,Zpiso,beta]])
+PuntosTri=np.array([[0.200,0,Zpiso,beta],[0.200,0.050,Zpiso,beta],[0.2433,0.025,Zpiso,beta],[0.200,0,Zpiso,beta]])
+PuntosR1=np.array([[0.200,-0.050,Zpiso,beta],[0.200,-0.100,Zpiso,beta]])
+PuntosR2=np.array([[0.225,-0.050,Zpiso,beta],[0.225,-0.100,Zpiso,beta]])
+PuntosR3=np.array([[0.250,-0.050,Zpiso,beta],[0.250,-0.100,Zpiso,beta]])
+PuntosEQTablero=np.array([[0.200,-0.125,Zpiso,beta],[0.210,-0.125,Zpiso,beta],[0.220,-0.125,Zpiso,beta],[0.230,-0.125,Zpiso,beta],[0.240,-0.125,Zpiso,beta],[0.250,-0.125,Zpiso,beta]])
+PuntosEQSeguridad=np.array([[0.200,-0.125,Zseguridad,beta],[0.210,-0.125,Zseguridad,beta],[0.220,-0.125,Zseguridad,beta],[0.230,-0.125,Zseguridad,beta],[0.240,-0.125,Zseguridad,beta],[0.250,-0.125,Zseguridad,beta]])
+
+#Qpuntos
+QPuntosA=ConvPosArr2QArr(PuntosA)
+QPuntosJ=ConvPosArr2QArr(PuntosJ)
+QPuntosTri=ConvPosArr2QArr(PuntosTri)
+QPuntosR1=ConvPosArr2QArr(PuntosR1)
+QPuntosR2=ConvPosArr2QArr(PuntosR2)
+QPuntosR3=ConvPosArr2QArr(PuntosR3)
+QPuntosEQTablero=ConvPosArr2QArr(PuntosEQTablero)
+QPuntosEQSeguridad=ConvPosArr2QArr(PuntosEQSeguridad)
+
+############################################################
+
 
 #Main
 if __name__ == '__main__':
@@ -124,7 +197,7 @@ if __name__ == '__main__':
 
         print("Bienvenido al laboratorio 5 de robótica!")
         print("Por favor espere a que el robot se dirija a home")
-        RutHome(PosHome)
+        RutHome(PosHomeS)
 
         PrintOpciones()
         caso=0
@@ -133,18 +206,47 @@ if __name__ == '__main__':
             caso=int(input())
             if caso==1:
                 print("Se seleccionó: Cargar herramienta")
+
+
+                time.sleep(0.5)
+                RutHome(PosHomeC)
             elif caso==2:
                 print("Se seleccionó: Espacio de trabajo")
+
+
+                time.sleep(0.5)
+                RutHome(PosHomeC)
             elif caso==3:
                 print("Se seleccionó: Dibujo de Iniciales")
+
+
+                time.sleep(0.5)
+                RutHome(PosHomeC)
             elif caso==4:
-                print("Se seleccionó: Dibujo de figuras geom´etricas")
+                print("Se seleccionó: Dibujo de figuras geométricas")
+
+
+                time.sleep(0.5)
+                RutHome(PosHomeC)
             elif caso==5:
                 print("Se seleccionó: Dibujo de puntos")
+
+
+                time.sleep(0.5)
+                RutHome(PosHomeC)
             elif caso==6:
                 print("Se seleccionó: Dibujo figura libre")
+
+
+                time.sleep(0.5)
+                RutHome(PosHomeC)
             elif caso==7:
                 print("Se seleccionó: Descarga de la herramienta")
+
+
+
+                time.sleep(0.5)
+                RutHome(PosHomeS)
             elif caso==8:
                 print("Se seleccionó: Salr")
             else:
